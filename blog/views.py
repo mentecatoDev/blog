4fb6d84104eb05d22db6b36492bfc56836275996
mentecatoDev <favila@iesromerovargas.com>
2021-02-16 15:12:42 +0100

    Termina punto 7.2 Actualizar

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -11,4 +11,15 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'Cintia'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
